601752f2a39e34c0094ccaa4c3529dba8cf6897b
betapie <ma.lauterbach@gmail.com>
2026-03-09 16:17:36 +0200

    add simple main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 mod core;
 
+use core::parser::Parser;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+    let filename = &args[1];
+    let f = std::fs::File::open(filename).unwrap();
+    let reader = std::io::BufReader::new(f);
+    let mut parser = Parser::new();
+    for line in reader.lines() {
+        parser.consume(line.unwrap()).unwrap();
+    }
+    let merge_file = parser.into_merge_file().unwrap();
+    let mut num_conflict_blocks = 0;
+    for block in &merge_file.blocks {
+        match block {
+            core::model::Block::Regular(_) => {}
+            core::model::Block::Conflict(_) => num_conflict_blocks += 1,
+        }
+    }
+    if num_conflict_blocks == 0 {
+        println!("No conflicts found in file {}", filename);
+    } else {
+        println!(
+            "Found {} conflicts in file {}",
+            num_conflict_blocks, filename
+        );
+    }
 }
